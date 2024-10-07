787d3f1b11fc7fff2ed5e214a4f8846c39bdb1cc
miiiwdy <fahmiwidyaa@gmail.com>
2024-10-07 17:14:08 +0700

    #2

--- profileViewer.js
+++ profileViewer.js
@@ -1,73 +1,71 @@
-const https = require("https");
-const readline = require("readline");
+const https = require('https');
+const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout
 });
 
-class ProfileView {
-  constructor() {
-    this.count = 0;
-  }
-
-  async viewProfile(url) {
-    const trimmedUrl = url.trim();
-    try {
-      await this.fetchUrl(trimmedUrl);
-      this.count++;
-      console.log(`\x1b[1mBoosting: \x1b[32m+${this.count}\x1b[0m`);
-    } catch (error) {
-      console.error(`ERR VIEW PROFILE: ${error}`);
+class ProfileViewer {
+    constructor() {
+        this.counter = 0;
     }
-  }
 
-  fetchUrl(url) {
-    return new Promise((resolve, reject) => {
-      https
-        .get(url, (res) => {
-          if (res.statusCode !== 200) {
-            return reject(`Request failed with status: ${res.statusCode}`);
-          }
-          res.on("data", () => {});
-          res.on("end", () => resolve());
-        })
-        .on("error", (err) => {
-          reject(`Network error: ${err.message} (code: ${err.code})`);
+    async viewProfile(url) {
+        const trimmedUrl = url.trim();
+        await this.get(trimmedUrl);
+        this.counter++;
+        console.log(`\x1b[1mConnecting!, Sending Boost: \x1b[32m+${this.counter}\x1b[0m`);
+    }
+
+    getCounter() {
+        return this.counter;
+    }
+
+    get(url) {
+        return new Promise((resolve, reject) => {
+            https.get(url, (res) => {
+                if (res.statusCode !== 200) {
+                    return reject(`Request failed with status code: ${res.statusCode}`);
+                }
+                res.on('data', () => {});
+                res.on('end', resolve);
+            }).on('error', reject);
         });
-    });
-  }
+    }
+
+    async viewProfilesInBatch(url, batchSize) {
+        const promises = [];
+        for (let i = 0; i < batchSize; i++) {
+            promises.push(this.viewProfile(url));
+        }
+        await Promise.all(promises);
+    }
 }
 
-const profileView = new ProfileView();
+const profileViewer = new ProfileViewer();
 
-rl.question("Enter GitHub username: ", (username) => {
-  const url = `https://visitcount.itsvg.in/api?id=${username}&label=Profile%20Views&color=8&icon=8&pretty=true`;
+rl.question('Enter GitHub Username: ', (username) => {
+    const url = `https://visitcount.itsvg.in/api?id=${username}&label=Profile%20Views&color=8&icon=8&pretty=true`;
 
-  rl.question("Enter number of views: ", async (numViews) => {
-    const startTime = Date.now();
+    rl.question('Enter number of View: ', async (views) => {
+        const timerStart = Date.now();
 
-    numViews = parseInt(numViews, 10);
-    if (isNaN(numViews) || numViews <= 0) {
-      console.log("Please enter a valid number of views.");
-      rl.close();
-      return;
-    }
+        const batchSize = 10; 
+        const parallelBatches = 4; 
+        const totalBatches = Math.ceil(views / batchSize);
+        
+        for (let i = 0; i < totalBatches; i += parallelBatches) {
+            const currentBatch = [];
+            for (let j = 0; j < parallelBatches && (i + j) < totalBatches; j++) {
+                currentBatch.push(profileViewer.viewProfilesInBatch(url, batchSize));
+            }
+            await Promise.all(currentBatch);
+        }
 
-    const parallelRequests = 10;
-    const tasks = Array.from({ length: numViews }, () => profileView.viewProfile(url));
-
-    for (let i = 0; i < tasks.length; i += parallelRequests) {
-      const chunk = tasks.slice(i, i + parallelRequests);
-      await Promise.all(chunk);
-    }
-
-    const endTime = Date.now();
-    const elapsedTime = (endTime - startTime) / 1000;
-
-    console.log(
-      `Boosting completed in \x1b[32m${elapsedTime}s\x1b[0m! Your GitHub account has an extra \x1b[32m${numViews}\x1b[0m Views!`
-    );
-    rl.close();
-  });
+        const timerStop = Date.now();
+        const timeElapsed = (timerStop - timerStart) / 1000;
+        console.log(`Completed boosting in \x1b[32m${timeElapsed}s\x1b[0m! ${github} GitHub account has now an extra \x1b[32m${views}\x1b[0m Views!`);
+        rl.close();
+    });
 });
